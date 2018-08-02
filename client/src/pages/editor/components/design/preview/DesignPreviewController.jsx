8268edde62b1f7851c8c207cd9d977870541ad43
tsxuehu <tsxuehu@gmail.com>
2018-08-02 22:35:13 +0800

    init

--- client/src/pages/editor/components/design/preview/DesignPreviewController.jsx
+++ client/src/pages/editor/components/design/preview/DesignPreviewController.jsx
@@ -1,182 +1,180 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, {PureComponent} from 'react';
 import cx from 'classnames';
 import {Pop} from 'zent';
 import pick from 'lodash/pick';
 import get from 'lodash/get';
-import { Draggable } from 'react-beautiful-dnd';
-import { DND_PREVIEW_CONTROLLER, DEFAULT_BACKGROUND } from './constants';
-import { ADD_COMPONENT_OVERLAY_POSITION } from '../constants';
+import {Draggable} from 'react-beautiful-dnd';
+import {DND_PREVIEW_CONTROLLER, DEFAULT_BACKGROUND} from './constants';
 
 class DesignPreviewController extends PureComponent {
 
-  static defaultProps = {
-    prefix: 'mp',
-  };
+    static defaultProps = {
+        prefix: 'mp',
+    };
 
-  render() {
-    const {
-      dragable,
-      configurable,
-      editable,
-      canDelete,
-      highlightWhenSelect,
-      isSelected,
-      component: PreviewComponent,
-      previewProps,
-      settings,
-      prefix,
-      id,
-      index,
-      allowHoverEffects,
-    } = this.props;
-    const props = pick(this.props, [
-      'value',
-      'design',
-      'settings',
-    ]);
-    const getClassName = highlight =>
-      cx(`${prefix}-design-preview-controller`, {
-        [`${prefix}-design-preview-controller--editable`]: editable,
-        [`${prefix}-design-preview-controller--selected`]: isSelected,
-        [`${prefix}-design-preview-controller--highlight`]: highlight,
-        [`${prefix}-design-preview-controller--dragable`]: dragable,
-      });
+    render() {
+        const {
+            dragable,
+            configurable,
+            editable,
+            canDelete,
+            highlightWhenSelect,
+            isSelected,
+            component: PreviewComponent,
+            previewProps,
+            settings,
+            prefix,
+            id,
+            index,
+            allowHoverEffects,
+        } = this.props;
+        const props = pick(this.props, [
+            'value',
+            'design',
+            'settings',
+        ]);
+        const getClassName = highlight =>
+            cx(`${prefix}-design-preview-controller`, {
+                [`${prefix}-design-preview-controller--editable`]: editable,
+                [`${prefix}-design-preview-controller--selected`]: isSelected,
+                [`${prefix}-design-preview-controller--highlight`]: highlight,
+                [`${prefix}-design-preview-controller--dragable`]: dragable,
+            });
 
-    const tree = dragable ? (
-      <Draggable
-        draggableId={id}
-        type={DND_PREVIEW_CONTROLLER}
-        isDragDisabled={!dragable}
-        index={index}
-      >
-        {(provided, snapshot) => {
-          // 拖拽的时候隐藏各种按钮，会很丑
-          const showButtons =
-            configurable && allowHoverEffects && !snapshot.isDragging;
-          const cls = getClassName(allowHoverEffects && highlightWhenSelect);
+        const tree = dragable ? (
+            <Draggable
+                draggableId={id}
+                type={DND_PREVIEW_CONTROLLER}
+                isDragDisabled={!dragable}
+                index={index}
+            >
+                {(provided, snapshot) => {
+                    // 拖拽的时候隐藏各种按钮，会很丑
+                    const showButtons =
+                        configurable && allowHoverEffects && !snapshot.isDragging;
+                    const cls = getClassName(allowHoverEffects && highlightWhenSelect);
 
-          return (
-            <div className={cls} onClick={this.onSelect}>
-              <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                style={{
-                  ...provided.draggableProps.style,
-                  backgroundColor: get(
-                    settings,
-                    'previewBackground',
-                    DEFAULT_BACKGROUND
-                  ),
+                    return (
+                        <div className={cls} onClick={this.onSelect}>
+                            <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                style={{
+                                    ...provided.draggableProps.style,
+                                    backgroundColor: get(
+                                        settings,
+                                        'previewBackground',
+                                        DEFAULT_BACKGROUND
+                                    ),
+                                }}
+                                className={`${prefix}-design-preview-controller__drag-handle`}
+                            >
+                                <PreviewComponent
+                                    prefix={prefix}
+                                    {...previewProps}
+                                    {...props}
+                                />
+                            </div>
+                            {provided.placeholder}
+
+                            {showButtons &&
+                            canDelete && (
+                                <DeleteButton prefix={prefix} onDelete={this.onDelete}/>
+                            )}
+                        </div>
+                    );
                 }}
-                className={`${prefix}-design-preview-controller__drag-handle`}
-              >
-                <PreviewComponent
-                  prefix={prefix}
-                  {...previewProps}
-                  {...props}
-                />
-              </div>
-              {provided.placeholder}
+            </Draggable>
+        ) : (
+            <div
+                className={getClassName(highlightWhenSelect)}
+                onClick={this.onSelect}
+            >
+                <div
+                    className={cx(
+                        `${prefix}-design-preview-controller__drag-handle`,
+                        `${prefix}-design-preview-controller__drag-handle--inactive`
+                    )}
+                >
+                    <PreviewComponent prefix={prefix} {...previewProps} {...props} />
+                </div>
 
-              {showButtons &&
+                {configurable &&
                 canDelete && (
-                  <DeleteButton prefix={prefix} onDelete={this.onDelete} />
+                    <DeleteButton prefix={prefix} onDelete={this.onDelete}/>
                 )}
             </div>
-          );
-        }}
-      </Draggable>
-    ) : (
-      <div
-        className={getClassName(highlightWhenSelect)}
-        onClick={this.onSelect}
-      >
-        <div
-          className={cx(
-            `${prefix}-design-preview-controller__drag-handle`,
-            `${prefix}-design-preview-controller__drag-handle--inactive`
-          )}
-        >
-          <PreviewComponent prefix={prefix} {...previewProps} {...props} />
-        </div>
+        );
 
-        {configurable &&
-          canDelete && (
-            <DeleteButton prefix={prefix} onDelete={this.onDelete} />
-          )}
-      </div>
-    );
-
-    return tree;
-  }
-
-  onSelect = evt => {
-    const { editable } = this.props;
-    if (!editable) {
-      return;
+        return tree;
     }
 
-    this.invokeCallback('onSelect', evt, false);
-  };
+    onSelect = evt => {
+        const {editable} = this.props;
+        if (!editable) {
+            return;
+        }
 
-  onDelete = () => {
-    this.invokeCallback('onDelete', null, true);
-  };
+        this.invokeCallback('onSelect', evt, false);
+    };
 
-  invokeCallback(action, evt, stopPropagation, ...args) {
-    if (stopPropagation && evt) {
-      evt.stopPropagation();
+    onDelete = () => {
+        this.invokeCallback('onDelete', null, true);
+    };
+
+    invokeCallback(action, evt, stopPropagation, ...args) {
+        if (stopPropagation && evt) {
+            evt.stopPropagation();
+        }
+
+        const {value} = this.props;
+        const cb = this.props[action];
+        cb && cb(value, ...args);
     }
-
-    const { value } = this.props;
-    const cb = this.props[action];
-    cb && cb(value, ...args);
-  }
 }
 
-function DeleteButton({ prefix, onDelete }) {
-  return (
-    <Pop
-      content="确定删除？"
-      trigger="click"
-      position="left-center"
-      centerArrow
-      onConfirm={onDelete}
-      wrapperClassName={`${prefix}-design-preview-controller__action-btn-delete`}
-    >
-      <IconDelete prefix={prefix} onClick={stopEventPropagation} />
-    </Pop>
-  );
+function DeleteButton({prefix, onDelete}) {
+    return (
+        <Pop
+            content="确定删除？"
+            trigger="click"
+            position="left-center"
+            centerArrow
+            onConfirm={onDelete}
+            wrapperClassName={`${prefix}-design-preview-controller__action-btn-delete`}
+        >
+            <IconDelete prefix={prefix} onClick={stopEventPropagation}/>
+        </Pop>
+    );
 }
 
 class IconDelete extends PureComponent {
-  render() {
-    const { prefix, onClick } = this.props;
-    return (
-      <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-        className={`${prefix}-design-preview-controller__icon-delete`}
-        onClick={onClick}
-      >
-        <g fill="none" fillRule="evenodd">
-          <circle cx="10" cy="10" r="10" />
-          <path
-            fill="#FFF"
-            d="M13.75 7.188l-.937-.938L10 9.063 7.188 6.25l-.938.937L9.062 10 6.25 12.812l.937.938L10 10.938l2.812 2.812.938-.937L10.938 10"
-          />
-        </g>
-      </svg>
-    );
-  }
+    render() {
+        const {prefix, onClick} = this.props;
+        return (
+            <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                className={`${prefix}-design-preview-controller__icon-delete`}
+                onClick={onClick}
+            >
+                <g fill="none" fillRule="evenodd">
+                    <circle cx="10" cy="10" r="10"/>
+                    <path
+                        fill="#FFF"
+                        d="M13.75 7.188l-.937-.938L10 9.063 7.188 6.25l-.938.937L9.062 10 6.25 12.812l.937.938L10 10.938l2.812 2.812.938-.937L10.938 10"
+                    />
+                </g>
+            </svg>
+        );
+    }
 }
 
 function stopEventPropagation(evt) {
-  evt && evt.stopPropagation();
+    evt && evt.stopPropagation();
 }
 
 export default DesignPreviewController;
